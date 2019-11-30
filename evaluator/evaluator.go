dfab295c6f96d0c618cd46c70f1291e4c609dc48
aha-oretama <sekine_y_529@msn.com>
2019-12-01 02:08:29 +0900

    4.6 add puts

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -13,18 +13,28 @@ var (
 )
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{Fn: func(args ...object.Object) object.Object {
-		if len(args) != 1 {
-			return newError("wrong number of arguments. got=%d, want=1", len(args))
-		}
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
 
-		switch arg := args[0].(type) {
-		case *object.String:
-			return &object.Integer{Value: int64(len(arg.Value))}
-		default:
-			return newError("argument to `len` not supported, got %s", arg.Type())
-		}
-	}},
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s", arg.Type())
+			}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Printf(arg.Inspect())
+			}
+			return NULL
+		},
+	},
 }
 
 func Eval(node ast.Node, env *object.Environment) object.Object {
